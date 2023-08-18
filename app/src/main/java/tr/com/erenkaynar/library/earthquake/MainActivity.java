75810204f1ca877e2693191e39aab875de1ae829
Şevket Eren KAYNAR <sqayner@gmail.com>
2023-08-18 19:06:19 +0300

    1.0.0

---
+++ app/src/main/java/tr/com/erenkaynar/library/earthquake/MainActivity.java
@@ -0,0 +1,14 @@
+package tr.com.erenkaynar.library.earthquake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
